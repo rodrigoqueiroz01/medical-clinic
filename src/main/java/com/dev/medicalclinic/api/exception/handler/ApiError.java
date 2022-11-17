a9ed2a0a1f6944159e084cf47f2557e694b3a67b
rodrigoqueiroz01 <rodrigoq1212@gmail.com>
2022-11-17 00:00:15 -0300

    Tratamento de exceções

--- src/main/java/com/dev/medicalclinic/api/exception/handler/ApiError.java
+++ src/main/java/com/dev/medicalclinic/api/exception/handler/ApiError.java
@@ -1,4 +1,40 @@
 package com.dev.medicalclinic.api.exception.handler;
 
-public class ApiError {
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import java.util.List;
+
+/**
+ * @author rodrigoqueiroz
+ */
+
+@Getter
+@Builder
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class ApiError implements Serializable {
+
+    @Serial
+    public static final long serialVersionUID = 1L;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy HH:mm:ss")
+    private final LocalDateTime timestamp = LocalDateTime.now();
+
+    private Integer status;
+    private String title;
+    private String detail;
+    private List<Field> fields;
+
+    @Getter
+    @Builder
+    public static class Field {
+        private String name;
+        private String message;
+    }
+
 }
--- src/main/java/com/dev/medicalclinic/api/exception/handler/ApiExceptionHandler.java
+++ src/main/java/com/dev/medicalclinic/api/exception/handler/ApiExceptionHandler.java
@@ -1,4 +1,243 @@
 package com.dev.medicalclinic.api.exception.handler;
 
-public class ApiExceptionHandler {
+import com.dev.medicalclinic.api.exception.DataAlreadyRegisteredException;
+import com.dev.medicalclinic.api.exception.DataNotFoundException;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.HttpServerErrorException;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import javax.persistence.NoResultException;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author rodrigoqueiroz
+ */
+
+@ControllerAdvice
+public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
+
+    private static final String INVALIDATION_MESSAGE = "Um ou mais campos estão inválidos! " +
+            "Faça o preenchimento correto e tente novamente.";
+
+    private static final String GENERIC_ERROR_MESSAGE = "Houve uma falha inesperada no sistema. " +
+            "Tente novamente e, se a falha persistir, entre em contato com o administrador.";
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+        return handleValidationException(ex, headers, status, request);
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleBindException(BindException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+        return handleValidationException(ex, headers, status, request);
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleExceptionInternal(Exception ex, Object body, HttpHeaders headers, HttpStatus status, WebRequest request) {
+        var apiError = ApiError
+                .builder()
+                .status(status.value())
+                .title(status.getReasonPhrase())
+                .build();
+
+        headers.setContentType(MediaType.APPLICATION_PROBLEM_JSON);
+
+        return super.handleExceptionInternal(ex, apiError, headers, status, request);
+    }
+
+    @ExceptionHandler(DataAlreadyRegisteredException.class)
+    protected ResponseEntity<Object> handleDataAlreadyRegistered(DataAlreadyRegisteredException ex, WebRequest request) {
+        var status = HttpStatus.BAD_REQUEST;
+
+        var apiError = ApiError
+                .builder()
+                .status(status.value())
+                .title("Dado único já cadastrado.")
+                .detail(ex.getMessage())
+                .build();
+
+        var headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_PROBLEM_JSON);
+
+        return super.handleExceptionInternal(ex, apiError, headers, status, request);
+    }
+
+    @ExceptionHandler(DataNotFoundException.class)
+    protected ResponseEntity<Object> handleDataNotFound(DataNotFoundException ex, WebRequest request) {
+        var status = HttpStatus.NOT_FOUND;
+
+        var apiError = ApiError
+                .builder()
+                .status(status.value())
+                .title("Recurso não encontrado.")
+                .detail(ex.getMessage())
+                .build();
+
+        var headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_PROBLEM_JSON);
+
+        return super.handleExceptionInternal(ex, apiError, headers, status, request);
+    }
+
+    @ExceptionHandler(NoResultException.class)
+    protected ResponseEntity<Object> handleNoResult(NoResultException ex, WebRequest request) {
+        var status = HttpStatus.NOT_FOUND;
+
+        var apiError = ApiError
+                .builder()
+                .status(status.value())
+                .title("Recurso não encontrado.")
+                .detail(ex.getMessage())
+                .build();
+
+        var headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_PROBLEM_JSON);
+
+        return super.handleExceptionInternal(ex, apiError, headers, status, request);
+    }
+
+    @ExceptionHandler(HttpClientErrorException.NotFound.class)
+    protected ResponseEntity<Object> handleNotFound(HttpClientErrorException.NotFound ex, WebRequest request) {
+        var status = HttpStatus.NOT_FOUND;
+
+        var apiError = ApiError
+                .builder()
+                .status(status.value())
+                .title("Recurso não encontrado.")
+                .detail(ex.getMessage())
+                .build();
+
+        var headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_PROBLEM_JSON);
+
+        return super.handleExceptionInternal(ex, apiError, headers, status, request);
+    }
+
+    @ExceptionHandler(HttpClientErrorException.BadRequest.class)
+    protected ResponseEntity<Object> handleNotFound(HttpClientErrorException.BadRequest ex, WebRequest request) {
+        var status = HttpStatus.BAD_REQUEST;
+
+        var apiError = ApiError
+                .builder()
+                .status(status.value())
+                .title("Recurso inválido.")
+                .detail(ex.getMessage())
+                .build();
+
+        var headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_PROBLEM_JSON);
+
+        return super.handleExceptionInternal(ex, apiError, headers, status, request);
+    }
+
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    protected ResponseEntity<Object> handleDataIntegrityViolation(DataIntegrityViolationException ex, WebRequest request) {
+        var status = HttpStatus.INTERNAL_SERVER_ERROR;
+
+        var apiError = ApiError
+                .builder()
+                .status(status.value())
+                .title("Erro de integridade de dados.")
+                .detail(ex.getMessage())
+                .build();
+
+        var headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_PROBLEM_JSON);
+
+        return super.handleExceptionInternal(ex, apiError, headers, status, request);
+    }
+
+    @ExceptionHandler(NullPointerException.class)
+    protected ResponseEntity<Object> handleNullPointer(NullPointerException ex, WebRequest request) {
+        var status = HttpStatus.INTERNAL_SERVER_ERROR;
+
+        var apiError = ApiError
+                .builder()
+                .status(status.value())
+                .title(GENERIC_ERROR_MESSAGE)
+                .detail(ex.getMessage())
+                .build();
+
+        var headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_PROBLEM_JSON);
+
+        return super.handleExceptionInternal(ex, apiError, headers, status, request);
+    }
+
+    @ExceptionHandler(HttpServerErrorException.class)
+    protected ResponseEntity<Object> handleHttpServerError(HttpServerErrorException ex, WebRequest request) {
+        var status = HttpStatus.INTERNAL_SERVER_ERROR;
+
+        var apiError = ApiError
+                .builder()
+                .status(status.value())
+                .title(GENERIC_ERROR_MESSAGE)
+                .detail(ex.getMessage())
+                .build();
+
+        var headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_PROBLEM_JSON);
+
+        return super.handleExceptionInternal(ex, apiError, headers, status, request);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<Object> handleUncaughtException(Exception ex, WebRequest request) {
+        var status = HttpStatus.INTERNAL_SERVER_ERROR;
+
+        var apiError = ApiError
+                .builder()
+                .status(status.value())
+                .title(GENERIC_ERROR_MESSAGE)
+                .build();
+
+        var headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_PROBLEM_JSON);
+
+        return super.handleExceptionInternal(ex, apiError, headers, status, request);
+    }
+
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<Object> handleRuntimeException(RuntimeException ex, WebRequest request) {
+        var status = HttpStatus.INTERNAL_SERVER_ERROR;
+
+        var apiError = ApiError
+                .builder()
+                .status(status.value())
+                .title(GENERIC_ERROR_MESSAGE)
+                .build();
+
+        var headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_PROBLEM_JSON);
+
+        return super.handleExceptionInternal(ex, apiError, headers, status, request);
+    }
+
+    private ResponseEntity<Object> handleValidationException(Exception ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+        List<ApiError.Field> fields = new ArrayList<>();
+
+        var apiError = ApiError
+                .builder()
+                .status(status.value())
+                .title(INVALIDATION_MESSAGE)
+                .detail("Preencha os dados que são obrigatórios, e respeite o limite/valor máximo dos caracteres.")
+                .fields(fields)
+                .build();
+
+        headers.setContentType(MediaType.APPLICATION_PROBLEM_JSON);
+
+        return super.handleExceptionInternal(ex, apiError, headers, status, request);
+    }
+
 }
