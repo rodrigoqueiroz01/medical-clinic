cd28eba6efbcf033b66863b0a19d75e6b87e2864
rodrigoqueiroz01 <rodrigoq1212@gmail.com>
2022-11-16 22:07:31 -0300

    Open API Config

--- src/main/java/com/dev/medicalclinic/api/config/OpenApiConfig.java
+++ src/main/java/com/dev/medicalclinic/api/config/OpenApiConfig.java
@@ -1,4 +1,9 @@
 package com.dev.medicalclinic.api.config;
 
-public class OpenApiConfig {
-}
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@OpenAPIDefinition(info = @Info(title = "API - Medical Clinic", version = "v1"))
+public class OpenApiConfig { }
